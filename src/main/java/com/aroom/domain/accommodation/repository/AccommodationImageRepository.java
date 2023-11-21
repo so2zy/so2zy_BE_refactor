f82d793917d0546ff534fc1fbcbeb92e4a728b7f
KwonMinwoo <qwerty029369@naver.com>
2023-11-21 14:35:14 +0900

    feat: image 엔티티의 repository 인터페이스 생성
    motivation:
    - 각 이미지들 엔티티들의 repository 생성이 누락되어 생성하게되었습니다.
    
    modification:
    - RoomImageRepository, AccommodationImageRepository 생성

---
+++ src/main/java/com/aroom/domain/accommodation/repository/AccommodationImageRepository.java
@@ -0,0 +1,8 @@
+package com.aroom.domain.accommodation.repository;
+
+import com.aroom.domain.accommodation.model.AccommodationImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccommodationImageRepository extends JpaRepository<AccommodationImage, Long> {
+
+}

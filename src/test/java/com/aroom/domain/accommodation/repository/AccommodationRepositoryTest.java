b3438645a10a9bc5332b10b97eaea1a99edc8163
Yurim Yang <yang980329@likelion.org>
2023-11-23 12:24:15 +0900

    test: Repository test 변경 motivation: - 자현님께서 남겨주신 피드백에 따라, ì repository test를 변경했습니다.
    modification:
    - @BeforeEach 구문 삭제 : save 로직이  없으므로 삭제
    - @AutoConfigureTestDatabase 삭제 : 이미 DataJpaTest가 기능을 갖고 있기에 삭제

--- src/test/java/com/aroom/domain/accommodation/repository/AccommodationRepositoryTest.java
+++ src/test/java/com/aroom/domain/accommodation/repository/AccommodationRepositoryTest.java
@@ -9,23 +9,15 @@ import com.aroom.domain.room.model.Room;
 import com.aroom.domain.room.model.RoomImage;
 import com.aroom.domain.room.repository.RoomImageRepository;
 import com.aroom.domain.room.repository.RoomRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class AccommodationRepositoryTest {
 
     @Autowired
@@ -39,36 +31,6 @@ public class AccommodationRepositoryTest {
 
     @Autowired
     private AccommodationImageRepository accommodationImageRepository;
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @BeforeEach
-    public void reset() {
-        entityManager.flush();
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
-        accommodationRepository.deleteAll();
-        roomRepository.deleteAll();
-        roomImageRepository.deleteAll();
-        accommodationImageRepository.deleteAll();
-        entityManager.createNativeQuery("TRUNCATE TABLE accommodation").executeUpdate();
-        entityManager.createNativeQuery("TRUNCATE TABLE room").executeUpdate();
-        entityManager.createNativeQuery("TRUNCATE TABLE room_image").executeUpdate();
-        entityManager.createNativeQuery("TRUNCATE TABLE accommodation_image").executeUpdate();
-        entityManager
-            .createNativeQuery("ALTER TABLE accommodation ALTER COLUMN `accommodation_id` RESTART WITH 1")
-            .executeUpdate();
-        entityManager
-            .createNativeQuery("ALTER TABLE room ALTER COLUMN `room_id` RESTART WITH 1")
-            .executeUpdate();
-        entityManager
-            .createNativeQuery("ALTER TABLE room_image ALTER COLUMN `room_image_id` RESTART WITH 1")
-            .executeUpdate();
-        entityManager
-            .createNativeQuery("ALTER TABLE accommodation_image ALTER COLUMN `accommodation_image_id` RESTART WITH 1")
-            .executeUpdate();
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
-    }
 
     @Test
     @DisplayName("지정된 숙소가 조회 됐습니다.")

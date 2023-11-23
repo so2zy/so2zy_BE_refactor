4fb7b90c1f79f84a15fad089eb894392178a95d5
Yurim Yang <yang980329@likelion.org>
2023-11-23 16:26:07 +0900

    feat: entity반환에서 dto반환으로 변경 및 프론트측 수정사항 반영 motivation: - 객실 image를 list가 아닌 url로 전달하기 위해 & entity 반환에서 dto 반환으로 변경하기 위해 RoomListInfoDTO생성했습니다. - 프론트측 1차 수정사항 반영했습니다.
    modification:
    -  RoomListInfoDTO에서 url을 String으로 받구현
    - ãAccommodationResponseDTO에서 RoomListInfoDTO로 반환해서 전달
    - AccommodationResponseDTO에서 ãid 추가 반환
    - RoomListInfoDTO에서 checkIn, checkOut 포맷팅 ì구현 & id 반환

--- src/main/java/com/aroom/domain/accommodation/dto/response/AccommodationResponseDTO.java
+++ src/main/java/com/aroom/domain/accommodation/dto/response/AccommodationResponseDTO.java
@@ -3,6 +3,8 @@ package com.aroom.domain.accommodation.dto.response;
 import com.aroom.domain.accommodation.model.Accommodation;
 import com.aroom.domain.accommodation.model.AccommodationImage;
 import com.aroom.domain.room.model.Room;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,36 +12,46 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class AccommodationResponseDTO {
 
+    private Long id;
     private String accommodationName;
     private Float latitude;
     private Float longitude;
     private String addressCode;
     private String phoneNumber;
     private List<AccommodationImage> accommodationImageList;
-    private List<Room> roomList;
+    private List<RoomListInfoDTO> roomInfoList;
 
     @Builder
-    public AccommodationResponseDTO(String accommodationName, Float latitude, Float longitude,
-        String addressCode, String phoneNumber, List<Room> roomList,
-        List<AccommodationImage> accommodationImageList) {
+    public AccommodationResponseDTO(long id, String accommodationName, Float latitude,
+        Float longitude, String addressCode, String phoneNumber,
+        List<AccommodationImage> accommodationImageList,
+        List<RoomListInfoDTO> roomInfoList) {
+        this.id = id;
         this.accommodationName = accommodationName;
         this.latitude = latitude;
         this.longitude = longitude;
         this.addressCode = addressCode;
         this.phoneNumber = phoneNumber;
         this.accommodationImageList = accommodationImageList;
-        this.roomList = roomList;
+        this.roomInfoList = roomInfoList;
     }
 
     public AccommodationResponseDTO(Accommodation accommodation) {
+        this.id = accommodation.getId();
         this.accommodationName = accommodation.getName();
         this.latitude = accommodation.getLatitude();
         this.longitude = accommodation.getLongitude();
         this.addressCode = accommodation.getAddressCode();
         this.phoneNumber = accommodation.getPhoneNumber();
         this.accommodationImageList = accommodation.getAccommodationImageList();
-        this.roomList = accommodation.getRoomList();
+        List<RoomListInfoDTO> roomInfoList = new ArrayList<>();
+        for (Room room : accommodation.getRoomList()) {
+            RoomListInfoDTO roomListInfoDTO = new RoomListInfoDTO(room);
+            roomInfoList.add(roomListInfoDTO);
+        }
+        this.roomInfoList = roomInfoList;
     }
 }

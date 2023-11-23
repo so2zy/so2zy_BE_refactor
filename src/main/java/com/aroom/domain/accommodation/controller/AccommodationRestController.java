7119caed68101e5a9a0b54baab8fdac8ce6e25ec
chadongmin <cdm2883@naver.com>
2023-11-23 13:56:48 +0900

    feat : AccommodationRestController 구현
    motivation:
    - 컨트롤러를 구현했습니다.
    modification:
    - 검색조건을 @ModelAttribute를 사용하여 매핑합니다. Pagination을 위해 Default값을 설정했습니다. 정렬 조건은 Default 값은 ASC로 설정했습니다.

---
+++ src/main/java/com/aroom/domain/accommodation/controller/AccommodationRestController.java
@@ -0,0 +1,53 @@
+package com.aroom.domain.accommodation.controller;
+
+import com.aroom.domain.accommodation.dto.AccommodationListResponse;
+import com.aroom.domain.accommodation.dto.OrderCondition;
+import com.aroom.domain.accommodation.dto.SearchCondition;
+import com.aroom.domain.accommodation.service.AccommodationService;
+import com.aroom.global.response.ApiResponse;
+import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/v1/accommodations")
+@RequiredArgsConstructor
+public class AccommodationRestController {
+
+    private final AccommodationService accommodationService;
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<List<AccommodationListResponse>>> findAllAccommodation(
+        @Nullable @ModelAttribute @Valid SearchCondition searchCondition,
+        @Nullable @RequestParam(defaultValue = "0") int page,
+        @Nullable @RequestParam(defaultValue = "10") int size
+    ) {
+
+        if (searchCondition != null) {
+            System.out.println(searchCondition.getOrderBy());
+            PageRequest pageRequest = PageRequest.of(page, size, Sort.by(
+                Sort.Direction.fromOptionalString(searchCondition.getOrderBy()).orElse(Direction.ASC),searchCondition.getOrderCondition()));
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(LocalDateTime.now(), "숙소 정보를 성공적으로 조회했습니다.",
+                    accommodationService.getAccommodationListBySearchCondition(searchCondition, pageRequest)));
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponse<>(LocalDateTime.now(), "숙소 정보를 성공적으로 조회했습니다.",
+                accommodationService.getAllAccommodation()));
+    }
+}

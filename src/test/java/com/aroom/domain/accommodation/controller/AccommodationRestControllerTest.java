7aeeb06269d8bd2832142329625b20a6deb492ee
Yurim Yang <yang980329@likelion.org>
2023-11-23 16:29:53 +0900

    fix: Dtest³경으로 Test변경 ãmotivation: - DTO의 구조 변경으로 Controller Test도 변경했습니다.
    modification:
    - ê°roomList에서 roomListInfoDTO를 갖는 LIST로 변경

--- src/test/java/com/aroom/domain/accommodation/controller/AccommodationRestControllerTest.java
+++ src/test/java/com/aroom/domain/accommodation/controller/AccommodationRestControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.aroom.domain.accommodation.dto.response.AccommodationResponseDTO;
+import com.aroom.domain.accommodation.dto.response.RoomListInfoDTO;
 import com.aroom.domain.accommodation.model.AccommodationImage;
 import com.aroom.domain.accommodation.service.AccommodationService;
 import com.aroom.domain.room.model.Room;
@@ -42,18 +43,9 @@ public class AccommodationRestControllerTest {
         @DisplayName("숙소 상세 정보를 조회할 수 있다.")
         void _willSuccess() throws Exception {
             // given
-            List<RoomImage> roomImageList = Arrays.asList(
-                RoomImage.builder().url(
-                        "https://www.lottehotel.com/content/dam/lotte-hotel/signiel/seoul/accommodation/premier/180708-30-2000-acc-seoul-signiel.jpg")
-                    .build(),
-                RoomImage.builder().url(
-                        "https://www.lottehotel.com/content/dam/lotte-hotel/signiel/seoul/accommodation/premier/2849-1-2000-roo-LTSG.jpg")
-                    .build()
-            );
-            List<Room> roomList = Arrays.asList(
-                Room.builder().type("DELUXE").price(350000).capacity(2).maxCapacity(4).checkIn(
-                        LocalTime.of(15, 0)).checkOut(LocalTime.of(11, 0)).stock(4)
-                    .roomImageList(roomImageList).build());
+            List<RoomListInfoDTO> roomList = Arrays.asList(
+                RoomListInfoDTO.builder().type("DELUXE").price(350000).capacity(2).maxCapacity(4).checkIn("15:00").checkOut("11:00").stock(4)
+                    .url("naver.com").build());
             List<AccommodationImage> accommodationImageList = Arrays.asList(
                 AccommodationImage.builder().url(
                         "https://www.lottehotel.com/content/dam/lotte-hotel/lotte/seoul/dining/restaurant/pierre-gagnaire/180711-33-2000-din-seoul-hotel.jpg.thumb.768.768.jpg")
@@ -62,7 +54,7 @@ public class AccommodationRestControllerTest {
             AccommodationResponseDTO accommodationResponseDTO = AccommodationResponseDTO.builder()
                 .accommodationName("롯데호텔").latitude(
                     (float) 150.54).longitude((float) 100.5).addressCode("서울특별시 중구 을지로 30")
-                .phoneNumber("02-771-1000").roomList(roomList)
+                .phoneNumber("02-771-1000").roomInfoList(roomList)
                 .accommodationImageList(accommodationImageList).build();
             given(accommodationService.getRoom(any(Long.TYPE))).willReturn(
                 accommodationResponseDTO);
@@ -71,7 +63,7 @@ public class AccommodationRestControllerTest {
             mockMvc.perform(get("/v1/accommodations/{accommodation_id}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.accommodationName").isString())
-                .andExpect(jsonPath("$.data.roomList").isArray())
+                .andExpect(jsonPath("$.data.roomInfoList").isArray())
                 .andExpect(jsonPath("$.data.accommodationImageList").isArray()).andDo(print());
             verify(accommodationService, times(1)).getRoom(any(Long.TYPE));
         }

945643fc9035a9cfa18f622a3c5c853323c39115
Yurim Yang <yang980329@likelion.org>
2023-11-24 16:25:40 +0900

    test: Transactional, ExtendWith 어노테이션 추가 motivation: - 타 api test시, memberServiceTest와 에러 발생
    modification:
    - @ãTransactional, @ExtendWith 추가

--- src/test/java/com/aroom/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/aroom/domain/member/service/MemberServiceTest.java
@@ -12,9 +12,14 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
 
     @InjectMocks

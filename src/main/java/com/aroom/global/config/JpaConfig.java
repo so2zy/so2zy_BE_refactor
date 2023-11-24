60ad3ce3d967a6b1571d4c2e838fbdf054b27a7d
chadongmin <cdm2883@naver.com>
2023-11-24 11:32:43 +0900

    refactor : 클래스명 수정하고, QueryFactory 빈 등록했습니다.

--- src/main/java/com/aroom/global/config/JpaConfig.java
+++ src/main/java/com/aroom/global/config/JpaConfig.java
@@ -1,10 +1,24 @@
 package com.aroom.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
 @Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.aroom")
 public class JpaConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 
 }

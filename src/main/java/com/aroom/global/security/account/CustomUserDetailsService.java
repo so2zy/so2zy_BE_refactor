f61e50d36e0d783bc7027fa592984b6738c2c905
Nine-JH <rnwkgus2@gmail.com>
2023-11-24 19:11:35 +0900

    refactor: 코드 수정
    motivation:
    - 테스트용 코드들을 삭제합니다.
    
    modification:

--- src/main/java/com/aroom/global/security/account/CustomUserDetailsService.java
+++ src/main/java/com/aroom/global/security/account/CustomUserDetailsService.java
@@ -3,16 +3,13 @@ package com.aroom.global.security.account;
 import com.aroom.domain.member.exception.MemberNotFoundException;
 import com.aroom.domain.member.model.Member;
 import com.aroom.domain.member.repository.MemberRepository;
-import java.util.List;
 import java.util.Set;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -24,9 +21,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        log.info("username: {}", username);
-        List<Member> all = memberRepository.findAll();
-        log.info("memberList: {}", all);
         Member member = memberRepository.findMemberByEmail(username)
             .orElseThrow(MemberNotFoundException::new);
 

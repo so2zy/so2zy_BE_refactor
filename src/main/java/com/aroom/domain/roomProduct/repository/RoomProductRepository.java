79079a0369a957082b10c5401de600acdb9b25cb
chadongmin <cdm2883@naver.com>
2023-11-26 14:30:36 +0900

    feat : 엔티티 구조를 수정
    motivation:
    - 테이블 구조 변경으로 인한 엔티티를 수정했습니다.
    modification:
    - RoomProduct 엔티티를 수정하고, 연관관계를 수정했습니다.

---
+++ src/main/java/com/aroom/domain/roomProduct/repository/RoomProductRepository.java
@@ -0,0 +1,8 @@
+package com.aroom.domain.roomProduct.repository;
+
+import com.aroom.domain.roomProduct.model.RoomProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomProductRepository extends JpaRepository<RoomProduct, Long> {
+
+}

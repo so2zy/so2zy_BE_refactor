f598015edb49ac7efafba35c78e3ef140eaaf6b5
Yurim Yang <yang980329@likelion.org>
2023-11-27 11:11:23 +0900

    feat: RoomProduct로부터 roomId를 가져오는 로직 추기
    motivation:
    - RoomId를 통해 RoomProduct를 가져오기 위해 메소드를 추가했습니다.
    
    modification:
    - findByRoomId 추가

--- src/main/java/com/aroom/domain/roomProduct/repository/RoomProductRepository.java
+++ src/main/java/com/aroom/domain/roomProduct/repository/RoomProductRepository.java
@@ -1,10 +1,12 @@
 package com.aroom.domain.roomProduct.repository;
 
 import com.aroom.domain.roomProduct.model.RoomProduct;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomProductRepository extends JpaRepository<RoomProduct, Long> {
 
+    Optional<RoomProduct> findByRoomId(Long roomId);
 }

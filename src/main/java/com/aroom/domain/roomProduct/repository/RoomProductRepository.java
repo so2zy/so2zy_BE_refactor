4c63d582f04536d11401efaef6c9d0757450c406
kwonMInwoo <qwerty029369@naver.com>
2023-11-27 12:35:21 +0900

    fix: 예약 기능 수정
    motivation:
    - roomProduct가 생겨 예약 기능의 수정이 생겼습니다.
    
    modification:
    - roomProduct의 날짜 별 방 리스트 불러오는 기능 추가
    - 예약 기능 전체 수정

--- src/main/java/com/aroom/domain/roomProduct/repository/RoomProductRepository.java
+++ src/main/java/com/aroom/domain/roomProduct/repository/RoomProductRepository.java
@@ -1,12 +1,23 @@
 package com.aroom.domain.roomProduct.repository;
 
+import com.aroom.domain.room.model.Room;
 import com.aroom.domain.roomProduct.model.RoomProduct;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomProductRepository extends JpaRepository<RoomProduct, Long> {
 
     Optional<RoomProduct> findByRoomId(Long roomId);
+
+    @Query("select rp from RoomProduct rp where rp.room = :room and (rp.startDate between :startDate and :endDate)")
+    List<RoomProduct> findByRoomAndBetweenStartDateAndEndDate(@Param("room")Room room, @Param("startDate")
+        LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }

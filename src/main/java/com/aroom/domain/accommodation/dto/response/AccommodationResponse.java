834fa9b942a9b99ae15b3177348b6459495c2474
Yurim Yang <yang980329@likelion.org>
2023-11-27 22:59:17 +0900

    feat: 주소, 찜유무 칼럼 추가 및 숙소->객실 로직 변경 motivation: - 주소, 찜 유무를 추가 조회하기 위해 필드를 추가했습니다. - 숙소->객실 로직에서 요청 인원보다 최대수용인원이 큰 방들로 분류하기 위해 filter를 걸었습니다. - 숫고->객실 로직에서 stock이 0보다 클 경우만 조회되기 위해 filter을 걸었습니다.
    modification:
    - address, favorite 필드 추가
    - filter추가

--- src/main/java/com/aroom/domain/accommodation/dto/response/AccommodationResponse.java
+++ src/main/java/com/aroom/domain/accommodation/dto/response/AccommodationResponse.java
@@ -4,8 +4,12 @@ import com.aroom.domain.accommodation.model.Accommodation;
 import com.aroom.domain.accommodation.model.AccommodationImage;
 import com.aroom.domain.room.model.Room;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,13 +24,15 @@ public class AccommodationResponse {
     private Float latitude;
     private Float longitude;
     private String addressCode;
+    private String address;
     private String phoneNumber;
     private String accommodationUrl;
+    private Boolean favorite;
     private List<RoomListInfoResponse> roomInfoList;
 
     @Builder
     public AccommodationResponse(Long id, String accommodationName, Float latitude,
-        Float longitude, String addressCode, String phoneNumber, String accommodationUrl,
+        Float longitude, String addressCode, String phoneNumber, String accommodationUrl,Boolean favorite,
         List<RoomListInfoResponse> roomInfoList) {
         this.id = id;
         this.accommodationName = accommodationName;
@@ -35,25 +41,27 @@ public class AccommodationResponse {
         this.addressCode = addressCode;
         this.phoneNumber = phoneNumber;
         this.accommodationUrl = accommodationUrl;
+        this.favorite = favorite;
         this.roomInfoList = roomInfoList;
     }
 
-    public AccommodationResponse(Accommodation accommodation, String startDate, Integer personnel, Long memberId) {
+    public AccommodationResponse(Accommodation accommodation, Integer personnel, Boolean isFavorite, Long betweenDays, LocalDate startDate) {
         this.id = accommodation.getId();
         this.accommodationName = accommodation.getName();
         this.latitude = accommodation.getLatitude();
         this.longitude = accommodation.getLongitude();
-        this.addressCode = accommodation.getAddressCode();
+        this.address = accommodation.getAddress();
         this.phoneNumber = accommodation.getPhoneNumber();
         this.accommodationUrl = accommodation.getAccommodationImageList().stream()
             .map(AccommodationImage::getUrl)
             .findFirst()
             .orElse(null);
-        List<RoomListInfoResponse> roomInfoList = new ArrayList<>();
-        for (Room room : accommodation.getRoomList()) {
-            RoomListInfoResponse roomListInfoResponse = new RoomListInfoResponse(room, startDate, personnel);
-            roomInfoList.add(roomListInfoResponse);
-        }
-        this.roomInfoList = roomInfoList;
+        this.favorite = isFavorite;
+        this.roomInfoList = accommodation.getRoomList().stream()
+            .filter(room -> room.getMaxCapacity() >= personnel)
+            .filter(room -> room.getRoomProductList().stream()
+                .allMatch(rp -> rp.getStock() > 0))
+            .map(room -> new RoomListInfoResponse(room,betweenDays,startDate))
+            .collect(Collectors.toList());
     }
 }

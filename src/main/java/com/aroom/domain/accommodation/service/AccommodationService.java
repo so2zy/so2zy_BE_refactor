0159a2fec40c49f10637fb7c1c9f3c3a12608728
Yurim Yang <yang980329@likelion.org>
2023-11-27 23:09:39 +0900

    feat: 숙소 상세 조회 서비스 로직 변경 motivation: - startDate와 endDate를 받아, 기간을 계산하여 원가에 기간별로 곱합니다. - findByAccommodationIdAndStartDate을 사용해서 startDate를 기준으로 startDate와 endDate기간 동안의 숙소를 찾습니다. - 만약, findByAccommodationIdAndStartDate을 사용해서 Accommodation이 안나온다면, Accommodation 정보만 보내주기 위해 Try-Catch문을 사용합니다. - findByAccommodationIdAndMemberId를 사용해서 Favorite 에 해당 Accommodation이 있는지 확인합니다. -
    modification:
    - ChronoUnit패키지의 DAYS를 사용해서 startDate, endDate 기간을 계산
    - findByAccommodationIdAndStartDate, findByAccommodationIdAndMemberId 사용

--- src/main/java/com/aroom/domain/accommodation/service/AccommodationService.java
+++ src/main/java/com/aroom/domain/accommodation/service/AccommodationService.java
@@ -1,15 +1,19 @@
 package com.aroom.domain.accommodation.service;
 
-import com.aroom.domain.accommodation.dto.response.AccommodationResponse;
-import com.aroom.domain.accommodation.exception.AccommodationNotFoundException;
-import com.aroom.domain.accommodation.model.Accommodation;
-import com.aroom.domain.accommodation.repository.AccommodationRepository;
-import lombok.RequiredArgsConstructor;
 import static com.aroom.domain.accommodation.controller.AccommodationRestController.NO_ORDER_CONDITION;
 
 import com.aroom.domain.accommodation.dto.AccommodationListResponse;
 import com.aroom.domain.accommodation.dto.SearchCondition;
+import com.aroom.domain.accommodation.dto.response.AccommodationOnlyResponse;
+import com.aroom.domain.accommodation.dto.response.AccommodationResponse;
+import com.aroom.domain.accommodation.exception.AccommodationNotFoundException;
+import com.aroom.domain.accommodation.model.Accommodation;
+import com.aroom.domain.accommodation.repository.AccommodationRepository;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -25,17 +29,34 @@ public class AccommodationService {
     private final AccommodationRepository accommodationRepository;
 
     @Transactional(readOnly = true)
-    public AccommodationResponse getRoom(Long accommodation_id, String startDate, String endDate, Integer personnel, Long member_id) {
-        Accommodation accommodation = accommodationRepository.findById(accommodation_id)
-            .orElseThrow(
-                AccommodationNotFoundException::new);
-        return new AccommodationResponse(accommodation,startDate,personnel, member_id);
+    public Object getRoom(Long accommodation_id, String startDate, String endDate,
+        Integer personnel, Long member_id) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate startDateTypeChanged = LocalDate.parse(startDate, formatter);
+        LocalDate endDateTypeChanged = LocalDate.parse(endDate, formatter);
+        long betweenDays = ChronoUnit.DAYS.between(startDateTypeChanged, endDateTypeChanged);
+
+        try {
+            Accommodation accommodation = accommodationRepository.findByAccommodationIdAndStartDate(
+                    accommodation_id, startDateTypeChanged, endDateTypeChanged)
+                .orElseThrow(AccommodationNotFoundException::new);
+            boolean isFavorite = accommodationRepository.findByAccommodationIdAndMemberId(
+                member_id, accommodation_id).isPresent();
+            return new AccommodationResponse(accommodation, personnel, isFavorite, betweenDays,
+                startDateTypeChanged);
+        } catch (AccommodationNotFoundException e) {
+            return new AccommodationOnlyResponse(
+                accommodationRepository.findById(accommodation_id).get(),
+                accommodationRepository.findByAccommodationIdAndMemberId(
+                    member_id, accommodation_id).isPresent());
+        }
     }
 
     @Transactional(readOnly = true)
     public List<AccommodationListResponse> getAccommodationListBySearchCondition(
         SearchCondition searchCondition, Pageable pageable, Sort sortCondition) {
-        if (sortCondition.toString().contains(NO_ORDER_CONDITION)){
+        if (sortCondition.toString().contains(NO_ORDER_CONDITION)) {
             List<Accommodation> accommodation = accommodationRepository.getAccommodationBySearchCondition(
                 searchCondition, pageable);
             return accommodation.stream()

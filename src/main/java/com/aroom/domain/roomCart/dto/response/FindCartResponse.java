e4a8454cfdf2165784733328e143caf981a5961b
KwonMinwoo <qwerty029369@naver.com>
2023-11-28 17:21:54 +0900

    test: 장바구니 조회 service 테스트 코드 작성
    motivation:
    - 장바구니 조회 기능을 테스트 할 수 있는 service의 테스트 코드를 작성했습니다.
    
    modification:
    - RoomCartServiceTest 코드의 조회를 테스트 하는 코드 추가
    - FindCartResponse 테스트에서 필요해서 Getter 추가

--- src/main/java/com/aroom/domain/roomCart/dto/response/FindCartResponse.java
+++ src/main/java/com/aroom/domain/roomCart/dto/response/FindCartResponse.java
@@ -1,10 +1,13 @@
-package com.aroom.domain.cart.dto.response;
+package com.aroom.domain.roomCart.dto.response;
 
 import com.aroom.domain.accommodation.dto.response.CartAccommodationResponse;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 public class FindCartResponse {
 
     private List<CartAccommodationResponse> accommodationList = new ArrayList<>();

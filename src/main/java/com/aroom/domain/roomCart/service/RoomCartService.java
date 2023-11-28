43a136746d69e1c68d8159cbfe8484b9eec1646a
Yurim Yang <yang980329@likelion.org>
2023-11-28 18:26:25 +0900

    feat: 객실 장바구니 등록 v2 구현
    motivation:
    - RoomProduct Entity 생성으로 객실 장바구니 등록 로직을 변경했습니다.
    
    modification:
    - Cart가 존재하지 않을 경우, Cart 생성
    - startDate부터 endDate까지의 roomProduct 조회
    - checkContinualDate : 해당 기간동안 roomProduct가 모두 List에 담겼는지 확인
    - findMinStockRoomProduct : 가장 적은 재고를 가진 RoomProduct를 찾는 메서드 구현
    - roomProduct의 stock에서 가장 적은 재고를 가진 roomProduct가 roomCart에 담긴 횟수를 빼서 0보다 크면 roomCart에 저장
    - roomCartRepository에 이를 저장하고 cart의 List에 추가

--- src/main/java/com/aroom/domain/roomCart/service/RoomCartService.java
+++ src/main/java/com/aroom/domain/roomCart/service/RoomCartService.java
@@ -2,30 +2,83 @@ package com.aroom.domain.roomCart.service;
 
 import com.aroom.domain.cart.model.Cart;
 import com.aroom.domain.cart.repository.CartRepository;
+import com.aroom.domain.member.exception.MemberNotFoundException;
+import com.aroom.domain.member.model.Member;
+import com.aroom.domain.member.repository.MemberRepository;
+import com.aroom.domain.roomCart.dto.request.RoomCartRequest;
 import com.aroom.domain.roomCart.dto.response.RoomCartResponse;
+import com.aroom.domain.roomCart.exception.OutOfStockException;
 import com.aroom.domain.roomCart.model.RoomCart;
 import com.aroom.domain.roomCart.repository.RoomCartRepository;
+import com.aroom.domain.roomProduct.exception.RoomProductNotFoundException;
 import com.aroom.domain.roomProduct.model.RoomProduct;
 import com.aroom.domain.roomProduct.repository.RoomProductRepository;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class RoomCartService {
 
     private final RoomProductRepository roomProductRepository;
     private final CartRepository cartRepository;
     private final RoomCartRepository roomCartRepository;
+    private final MemberRepository memberRepository;
 
-    public RoomCartResponse postRoomCart(Long member_id, Long room_id) {
-        RoomProduct roomProduct = roomProductRepository.findByRoomId(room_id).get();
-        Cart cart = cartRepository.findByMemberId(member_id).get();
-        RoomCart roomCart = RoomCart.builder().cart(cart).roomProduct(roomProduct).build();
-        roomCartRepository.save(roomCart);
-        cart.postRoomCarts(roomCart);
+    @Transactional
+    public RoomCartResponse postRoomCart(Long member_id, Long room_id,
+        RoomCartRequest roomCartRequest) {
+
+        Optional<Cart> optionalCart = cartRepository.findByMemberId(member_id);
+        Cart cart = optionalCart.orElseGet(() -> {
+            Member member = memberRepository.findById(member_id).orElseThrow(
+                MemberNotFoundException::new);
+            return cartRepository.save(new Cart(member));
+        });
+
+        List<RoomProduct> roomProductList = roomProductRepository.findByRoomIdAndStartDateAndEndDate(
+            room_id,
+            roomCartRequest.getStartDate(), roomCartRequest.getEndDate());
+
+        checkContinualDate(roomProductList, roomCartRequest);
+
+        RoomProduct roomProductMinStock = findMinStockRoomProduct(roomProductList);
+
+        for (RoomProduct roomProduct : roomProductList) {
+            List<RoomCart> roomCartList = roomCartRepository.findByRoomProductId(
+                roomProductMinStock.getId());
+            if (roomProduct.getStock() - roomCartList.size() > 0) {
+                RoomCart roomCart = RoomCart.builder().cart(cart).roomProduct(roomProduct).build();
+                roomCartRepository.save(roomCart);
+                cart.postRoomCarts(roomCart);
+            } else {
+                throw new OutOfStockException();
+            }
+        }
         return new RoomCartResponse(cart);
+    }
+
+    private void checkContinualDate(List<RoomProduct> roomProductList,
+        RoomCartRequest roomCartRequest) {
+        LocalDate startDate = roomCartRequest.getStartDate();
+        LocalDate endDate = roomCartRequest.getEndDate();
+        long betweenDays = ChronoUnit.DAYS.between(startDate, endDate);
+        if (roomProductList.size() != betweenDays) {
+            throw new RoomProductNotFoundException();
+        }
+    }
+
+    private RoomProduct findMinStockRoomProduct(List<RoomProduct> roomProductList) {
+        int minStock = Integer.MAX_VALUE;
+        for (RoomProduct roomProduct : roomProductList) {
+            minStock = Math.min(roomProduct.getStock(), minStock);
+        }
+        RoomProduct minStockRoomProduct = roomProductRepository.findByStock(minStock).get();
+        return minStockRoomProduct;
     }
 }

470e3f9683ab03f631a925b64c587f67da6659a7
chadongmin <cdm2883@naver.com>
2023-11-28 13:43:50 +0900

    Refactoring : 응답 DTO 수정에 따른 내부 로직 수정 및 보일러 플레이트 코드 제거
    motivation:
    - 내부 로직을 수정하고 중복된 코드를 제거했습니다.
    modification :
    - 중복 코드를 메서드로 분리하고 코드를 간결하게 수정했습니다.

--- src/main/java/com/aroom/domain/accommodation/service/AccommodationService.java
+++ src/main/java/com/aroom/domain/accommodation/service/AccommodationService.java
@@ -16,7 +16,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @Slf4j
 @Transactional

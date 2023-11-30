c8327c44817a6e2a4386d95b6c3e6d1880fef87a
kwonMInwoo <qwerty029369@naver.com>
2023-11-30 16:46:34 +0900

    fix: reservation room image 수정
    motivation:
    - 예약 response에서 image를 잘못된 id값으로 찾아 반환하고 있던것을 수정했습니다.
    
    modification:
    - reservation service에서 roomImage find 수정

--- src/main/java/com/aroom/domain/room/repository/RoomImageRepository.java
+++ src/main/java/com/aroom/domain/room/repository/RoomImageRepository.java
@@ -1,8 +1,9 @@
 package com.aroom.domain.room.repository;
 
 import com.aroom.domain.room.model.RoomImage;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
-
+    Optional<RoomImage> findByRoomId(Long roomId);
 }

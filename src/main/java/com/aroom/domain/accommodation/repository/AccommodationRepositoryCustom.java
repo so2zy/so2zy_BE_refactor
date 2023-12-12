2316fc66a974403553f67fc4857a03a9189df5f8
cdm2883@naver.com <cdm2883@naver.com>
2023-12-12 10:23:48 +0900

    refactoring : 불필요한 import 삭제

--- src/main/java/com/aroom/domain/accommodation/repository/AccommodationRepositoryCustom.java
+++ src/main/java/com/aroom/domain/accommodation/repository/AccommodationRepositoryCustom.java
@@ -1,13 +1,9 @@
 package com.aroom.domain.accommodation.repository;
 
 import com.aroom.domain.accommodation.dto.AccommodationListResponse;
-import com.aroom.domain.accommodation.dto.AccommodationResponse;
 import com.aroom.domain.accommodation.dto.SearchCondition;
-import com.aroom.domain.accommodation.model.Accommodation;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 @Repository

48ace197e86976a3d56402a4c9227a3fc322883b
cdm2883@naver.com <cdm2883@naver.com>
2023-12-12 10:23:27 +0900

    fix : Mockito에서 Pageable의 기본 생성자를 인식하지 못해서 @PageableDefault에서 @RequestParam으로 수정

--- src/main/java/com/aroom/domain/accommodation/controller/AccommodationRestController.java
+++ src/main/java/com/aroom/domain/accommodation/controller/AccommodationRestController.java
@@ -1,24 +1,16 @@
 package com.aroom.domain.accommodation.controller;
 
 import com.aroom.domain.accommodation.dto.AccommodationListResponse;
-import com.aroom.domain.accommodation.dto.AccommodationResponse;
 import com.aroom.domain.accommodation.dto.SearchCondition;
-import com.aroom.domain.accommodation.repository.AccommodationRepositoryImpl;
 import com.aroom.domain.accommodation.service.AccommodationService;
 import com.aroom.global.resolver.Login;
 import com.aroom.global.resolver.LoginInfo;
 import com.aroom.global.response.ApiResponse;
 import jakarta.annotation.Nullable;
-import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,12 +31,12 @@ public class AccommodationRestController {
     @GetMapping("")
     public ResponseEntity<ApiResponse<AccommodationListResponse>> findAccommodation(
         @Nullable @ModelAttribute SearchCondition searchCondition,
-        @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable
+        @RequestParam(defaultValue = "0") Integer page,
+        @RequestParam(defaultValue = "10") Integer size
 
     ) {
         PageRequest pageRequest = PageRequest.of(
-            pageable.getPageNumber(),
-            pageable.getPageSize());
+            page, size);
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(new ApiResponse<>(LocalDateTime.now(), "숙소 정보를 성공적으로 조회했습니다.",
